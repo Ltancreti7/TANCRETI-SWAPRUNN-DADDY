3a145593a13bf273901f0e3d03077a14ee6f5d37
Luke Tancreti <ltancreti7@gmail.com>
2025-12-03 12:51:29 -0500

    Correct bundle ID applied to native iOS project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,10 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tancreti.swaprunn',
-  appName: 'SwapRunn',
-  webDir: 'dist'
+  appId: 'com.tancreti.swaprunn.daddy',
+  appName: 'SwapRunn Daddy Mobile',
+  webDir: 'dist',
+  bundledWebRuntime: false
 };
 
 export default config;
